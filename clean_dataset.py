2005deab7dc0abe9027a6b5e878f7dcf1c17c522
Elisa Ottoboni <elisaottoboni@MacBook-Air-di-Elisa-2.local>
2024-06-23 01:30:05 +0200

    Creation of new tables based on the analysis made on the 'Online_Retail_Sales_colab.ipynb' file

--- clean_dataset.py
+++ clean_dataset.py
@@ -4,73 +4,78 @@ import numpy as np
 def return_clean_dataset():
     answer = input("Do you want to maintain the row with NaN value (yes/no): ").lower()
     if answer == "no":
-        sales_df = pd.read_csv('delete_row.csv')
+        df_sales = pd.read_csv('delete_row.csv')
     elif answer == "yes":
-        sales_df = pd.read_csv('change_value.csv')
+        answer = input("Press 1 if you want to delete CustomerId column"
+                        "\n Press 2 if you want to assign random number to CustomerID"
+                        "\n Press 3 if you want to replace NaN value with '00000' value: ").lower()
+        if answer == 1:
+            df_sales = pd.read_csv('erased_CustomerID.csv')
+        elif answer == 2:
+            df_sales = pd.read_csv('change_random_value.csv')
+        elif answer == 3:
+            df_sales = pd.read_csv('change_00000_value.csv')
+        else:
+            print('Wrong answer')
     else:
         print('Wrong answer')
-    return sales_df  
+    return df_sales  
 
 
 def delete_row():
-    sales_df = pd.read_csv('online_retail.csv')
+    df_sales = pd.read_csv('online_retail.csv')
     
-    sales_df.dropna(inplace = True)
-    sales_df.reset_index(drop = True, inplace = True)
-    return sales_df
+    df_sales.dropna(inplace = True)
+    df_sales.to_csv('delete_row.csv', index=False)
 
 
-def change_value():
-    sales_df = pd.read_csv('online_retail.csv')
+def change_description():
+    df_sales = pd.read_csv('online_retail.csv')
     
-    sales_df.Description.fillna('NO DESCRIPTION', inplace = True)
-    unique_CustomerID_mask = sales_df.CustomerID.unique()
-    customerId_mask = sales_df.CustomerID.isna()
-    InvoiceNo_without_CustomerID = sales_df.InvoiceNo[customerId_mask]
-    new_CustomerID = generate_random_numbers(InvoiceNo_without_CustomerID.nunique(), unique_CustomerID_mask)
-    unique_InvoiceNo_with_NaN = InvoiceNo_without_CustomerID.unique()
-    for invoice in unique_InvoiceNo_with_NaN:
-        # Randomly select one of the numbers from the array
-        new_customerID = np.random.choice(new_CustomerID)
-        # Assign the new CustomerID to the rows with the same InvoiceNo
-        sales_df.loc[(sales_df['InvoiceNo'] == invoice) & customerId_mask, 'CustomerID'] = new_customerID
-    return sales_df
+    NaN_description_unique = df_sales[df_sales['Description'].isnull()]['StockCode'].unique()
+    description_unique = df_sales[df_sales['Description'].notna()]['StockCode'].unique()
+    for SC in NaN_description_unique:
+        if SC in description_unique:
+            description_mapping = df_sales.loc[df_sales['StockCode'] == SC, 'Description'].value_counts().idxmax()
+            df_sales.loc[(df_sales['StockCode'] == SC) & (df_sales['Description'].isna()), 'Description'] = description_mapping
+        else:
+            df_sales.loc[df_sales['StockCode'] == SC, 'Description'] = 'NO DESCRIPTION'
+    return df_sales
+
+
+def erased_CustomerID():
+    df_sales = change_description()
+    
+    df_sales.drop(columns = ['CustomerID'], inplace = True)
+    df_sales.to_csv('erased_CustomerID.csv', index = False)
 
 
 def generate_random_numbers(length, existing_numbers):
     generated_numbers = []
-    # Generate random numbers until the length of the array reaches the desired length
     while len(generated_numbers) < length:
         random_number = np.random.randint(10000, 20000)
-        # Ensure that the randomly generated number is not already present in the existing_numbers array
         if random_number not in existing_numbers:
             generated_numbers.append(random_number)
     return generated_numbers
 
 
-import tkinter as tk
-from tkinter import simpledialog
-
-# Funzione per visualizzare la finestra di dialogo di input al centro dello schermo
-def mostra_input():
-    root = tk.Tk()
-    root.withdraw()  # Nasconde la finestra principale
-
-    # Ottieni le dimensioni dello schermo
-    larghezza_schermo = root.winfo_screenwidth()
-    altezza_schermo = root.winfo_screenheight()
-
-    # Crea la finestra di dialogo di input
-    input_utente = simpledialog.askstring("Input", "Inserisci il testo:")
-
-    # Calcola le coordinate per centrare la finestra di dialogo
-    x = larghezza_schermo // 2 - 150
-    y = altezza_schermo // 2 - 50
-
-    # Posiziona la finestra di dialogo al centro dello schermo
-    root.geometry(f"300x100+{x}+{y}")
-
-    # Mostra la finestra di dialogo di input
-    root.mainloop()
+def change_random_value():
+    df_sales = change_description()
+    
+    unique_CustomerID_mask = df_sales.CustomerID.unique()
+    customerId_mask = df_sales.CustomerID.isna()
+    InvoiceNo_without_CustomerID = df_sales.InvoiceNo[customerId_mask]
+    new_CustomerID = generate_random_numbers(InvoiceNo_without_CustomerID.nunique(), unique_CustomerID_mask)
+    unique_InvoiceNo_with_NaN = InvoiceNo_without_CustomerID.unique()
+    for invoice in unique_InvoiceNo_with_NaN:
+        new_customerID = np.random.choice(new_CustomerID)
+        df_sales.loc[(df_sales['InvoiceNo'] == invoice) & customerId_mask, 'CustomerID'] = new_customerID
+    df_sales.to_csv('change_random_value.csv', index = False)
 
 
+
+def change_00000_value():
+    df_sales = change_description()
+    
+    df_sales.loc[df_sales['CustomerID'].isna(), 'CustomerID'] = 00000.0
+    df_sales.to_csv('change_00000_value.csv', index = False)
