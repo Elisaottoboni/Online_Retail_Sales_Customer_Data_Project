ecf63ed4b6b78584b9a9e100bdaab9df1e1b0962
Elisaottoboni <elisa.ottoboni@studenti.univr.it>
2024-06-27 18:53:59 +0200

    Update various files and add new datasets

--- clean_dataset.py
+++ clean_dataset.py
@@ -1,36 +1,8 @@
 import pandas as pd
 import numpy as np
 
-def return_clean_dataset():
-    answer = input("Do you want to maintain the row with NaN value (yes/no): ").lower()
-    if answer == "no":
-        df_sales = pd.read_csv('delete_row.csv')
-    elif answer == "yes":
-        answer = input("Press 1 if you want to delete CustomerId column"
-                        "\n Press 2 if you want to assign random number to CustomerID"
-                        "\n Press 3 if you want to replace NaN value with '00000' value: ").lower()
-        if answer == 1:
-            df_sales = pd.read_csv('erased_CustomerID.csv')
-        elif answer == 2:
-            df_sales = pd.read_csv('change_random_value.csv')
-        elif answer == 3:
-            df_sales = pd.read_csv('change_00000_value.csv')
-        else:
-            print('Wrong answer')
-    else:
-        print('Wrong answer')
-    return df_sales  
-
-
-def delete_row():
-    df_sales = pd.read_csv('online_retail.csv')
-    
-    df_sales.dropna(inplace = True)
-    df_sales.to_csv('delete_row.csv', index=False)
-
-
-def change_description():
-    df_sales = pd.read_csv('online_retail.csv')
+def clean_dataset():
+    df_sales = pd.read_csv('dataset/online_retail.csv')
     
     NaN_description_unique = df_sales[df_sales['Description'].isnull()]['StockCode'].unique()
     description_unique = df_sales[df_sales['Description'].notna()]['StockCode'].unique()
@@ -40,42 +12,37 @@ def change_description():
             df_sales.loc[(df_sales['StockCode'] == SC) & (df_sales['Description'].isna()), 'Description'] = description_mapping
         else:
             df_sales.loc[df_sales['StockCode'] == SC, 'Description'] = 'NO DESCRIPTION'
+    
+    df_sales = df_sales.dropna()
+
+    df_sales["InvoiceDate"] = pd.to_datetime(df_sales["InvoiceDate"])
+    df_sales["Time"] = df_sales["InvoiceDate"].dt.time
+    df_sales["Day"] = df_sales["InvoiceDate"].dt.day
+    df_sales["Month"] = df_sales["InvoiceDate"].dt.month
+    df_sales["Year"] = df_sales["InvoiceDate"].dt.year
+
+    df_sales['TotalSales'] = df_sales['Quantity'] * df_sales['UnitPrice']
+
+    df_sales['Season'] = df_sales.Month.apply(get_month)
+
+    df_sales = df_sales[df_sales['Quantity'] > 0]
+
+    df_sales.to_csv(f'{'dataset'}/{'clean_dataset.csv'}', index=False)
     return df_sales
 
 
-def erased_CustomerID():
-    df_sales = change_description()
-    
-    df_sales.drop(columns = ['CustomerID'], inplace = True)
-    df_sales.to_csv('erased_CustomerID.csv', index = False)
+def get_month(month):
+  if month in [3, 4, 5]:
+    return 'Spring'
+  elif month in [6, 7, 8]:
+    return 'Summer'
+  elif month in [9, 10, 11]:
+    return 'Autumn'
+  elif month in [12, 1, 2]:
+    return 'Winter'
+  else:
+    return 'Unknown'
+
+clean_dataset()
 
 
-def generate_random_numbers(length, existing_numbers):
-    generated_numbers = []
-    while len(generated_numbers) < length:
-        random_number = np.random.randint(10000, 20000)
-        if random_number not in existing_numbers:
-            generated_numbers.append(random_number)
-    return generated_numbers
-
-
-def change_random_value():
-    df_sales = change_description()
-    
-    unique_CustomerID_mask = df_sales.CustomerID.unique()
-    customerId_mask = df_sales.CustomerID.isna()
-    InvoiceNo_without_CustomerID = df_sales.InvoiceNo[customerId_mask]
-    new_CustomerID = generate_random_numbers(InvoiceNo_without_CustomerID.nunique(), unique_CustomerID_mask)
-    unique_InvoiceNo_with_NaN = InvoiceNo_without_CustomerID.unique()
-    for invoice in unique_InvoiceNo_with_NaN:
-        new_customerID = np.random.choice(new_CustomerID)
-        df_sales.loc[(df_sales['InvoiceNo'] == invoice) & customerId_mask, 'CustomerID'] = new_customerID
-    df_sales.to_csv('change_random_value.csv', index = False)
-
-
-
-def change_00000_value():
-    df_sales = change_description()
-    
-    df_sales.loc[df_sales['CustomerID'].isna(), 'CustomerID'] = 00000.0
-    df_sales.to_csv('change_00000_value.csv', index = False)
--- online_retail_streamlit.py
+++ online_retail_streamlit.py
@@ -4,17 +4,21 @@
 # Libraries
 #####################################################
 
-import pandas as pd
-import numpy as np
-import clean_dataset as cl # I import the Python file that cleans the dataframe
-import streamlit as st
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import geopandas as gpd
+
+#visualization
+import seaborn as sns
 import matplotlib.pyplot as plt
-import seaborn as sb
-import squarify
+
+#machine learning
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, PolynomialFeatures
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from sklearn.linear_model import LinearRegression
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, classification_report, precision_score, recall_score
+from sklearn.ensemble import GradientBoostingRegressor, RandomForestRegressor
+from prettytable import PrettyTable
 
 
 #####################################################
@@ -26,39 +30,112 @@ df_sales = pd.read_csv('dataset/online_retail.csv')
 #####################################################
 # Functions
 #####################################################
+# Functions for the plot of the outlier
+def box_and_scatter_plot(df, df_column):
+    fig, axs = plt.subplots(1, 2, figsize = (10, 5))
 
-# Function to visualize text to web page
-def write_text(text):
-    st.text(text)
+    # Create a box plot in the first subplot
+    sns.boxplot(x=df[df_column], ax = axs[0])
+    axs[0].set_xlabel(df_column)
+    axs[0].set_title('Box Plot of ' + df_column + ' to Identify Outliers')
 
-# Function to visualize simple text to web page
-def generic_write(text):
-    st.write(text)
+    # Create a scatter plot in the second subplot
+    sns.scatterplot(x = df.index, y = df[df_column], ax=axs[1])
+    axs[1].set_xlabel('Index')
+    axs[1].set_ylabel(df_column)
+    axs[1].set_title('Scatter Plot of ' + df_column + ' to Identify Outliers')
 
-# Function to visualize dataframe to web page
-def write_df(df):
-    st.dataframe(df)
+    # Adjust the layout
+    plt.tight_layout()
 
-# Function to create a subheader to web page
-def write_subheader(text):
-    st.subheader(text)
+    # Show the plots
+    plt.show()
 
-# Function to create a title to web page
-def write_title(text):
-    st.title(text)
 
-# Function to create a title to web page
-def write_header(text):
-    st.header(text)
+# Function to encod the object valu of the dataset
+def encoding_cl(df, df_col):
+  label_encoder = LabelEncoder()
+  for column in df_col:
+    if df[column].dtype == 'object':
+      df[column] = label_encoder.fit_transform(df[column])
+  return df
 
-# Function to write markdown code to web page
-def write_md(text):
-    st.markdown(text)
+# 
+def prepare_data(df, target, columns_to_exclude=[]):
+    # Drop 'time' column if present
+    if 'InvoiceDate' in df.columns:
+        df = df.drop(columns=['InvoiceDate'])
 
-# Function to write caption to web page
-def write_caption(text):
-    st.caption(text)
+    # Dynamically select features by excluding the columns we don't need
+    features = [col for col in df.columns if col not in columns_to_exclude + [target]]
 
+    X = df[features]
+    y = df[target]
+
+    return X, y
+
+
+def train_model(X_train, y_train, model):
+    model.fit(X_train, y_train)
+    return model
+
+
+def train_and_evaluate(df, target, model, columns_to_exclude=[]):
+    X, y = prepare_data(df, target, columns_to_exclude)
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    #print the shape of our train and testing sets
+    table_shape = PrettyTable()
+    table_shape.field_names = ["", "X_shape", "y_shape"]
+    table_shape.add_row(["Training set shape", X_train.shape, y_train.shape])
+    table_shape.add_row(["Testing set shape", X_test.shape, y_test.shape])
+    print(table_shape)
+
+    model_train = train_model(X_train, y_train, model)
+
+    evaluate_and_plot(X_test, y_test, model_train, target)
+
+
+def evaluate_and_plot(X_test, y_test, model, target):
+    # Make predictions
+    y_pred = model.predict(X_test)
+
+    # Evaluate the model
+    MSE = mean_squared_error(y_test, y_pred)
+    MAE = mean_absolute_error(y_test, y_pred)
+    R2_Score = r2_score(y_test, y_pred)
+    # intercept = model.intercept_
+
+    results = PrettyTable()
+    results.field_names = ["Metric", "Value"]
+    results.add_row(["Mean Squared Error", MSE])
+    results.add_row(["Mean Absolute Error", MAE])
+    results.add_row(["R2 Score", R2_Score])
+    # results.add_row(["Intercept", intercept])
+    print(results)
+
+    # Plotting the results
+    plt.figure(figsize=(10, 6))
+    plt.plot(y_test.values[:100], label='Actual')
+    plt.plot(y_pred[:100], label='Predicted')
+    plt.xlabel('Sample')
+    plt.ylabel(target)
+    plt.title(f'Actual vs Predicted {target}')
+    plt.legend()
+    plt.show()
+
+
+def train_and_evaluate_polynomial(X, y, model):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
+    
+    table_shape = PrettyTable()
+    table_shape.field_names = ["", "X_shape", "y_shape"]
+    table_shape.add_row(["Training set shape", X_train.shape, y_train.shape])
+    table_shape.add_row(["Testing set shape", X_test.shape, y_test.shape])
+    print(table_shape)
+
+    trained_model = train_model(X_train, y_train, model)
+    evaluate_and_plot(X_test, y_test, trained_model, target)
 #####################################################
 # Main
 #####################################################
@@ -102,7 +179,7 @@ if st.sidebar.checkbox('EDA'):
     ###################
     # AFTER CLEANING
     st.header('After Cleaning')
-    #df_sales = cl.return_clean_dataset()
+    df_sales = cld.clean_dataset()
     st.write('Online Retail dataframe:')
     st.write(df_sales)
     st.write('Rows and columns:', df_sales.shape)
